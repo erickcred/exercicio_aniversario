178ca29bf51cd3560e89e3b4d233f30b7969271a
Erick Rick <erickcredjes@gmail.com>
2023-02-27 14:25:14 -0300

    Incluindo form contato
    Inclusão do forms para contato e validação do mesmo com jQuery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,52 @@
-const dataEvento = new Date("Jul 05, 2023 09:00:00");
-const timeStampEvento = dataEvento.getTime();
+$(document).ready(function() {
+    const dataEvento = new Date("Jul 05, 2023 09:00:00");
+    const timeStampEvento = dataEvento.getTime();
+    
+    let evento = setInterval(function() {
+        const dataAtual = new Date();
+        const timeStampDataAtual = dataAtual.getTime();
+        const tempoParaEvento = timeStampEvento - timeStampDataAtual;
+        
+        if (tempoParaEvento <= 0) {
+            $(".hero__content").text("Evento finalizado");
+            clearInterval(evento);
+        } else {
+            mostraTempoEmTela(tempoParaEvento);
+        }
+    }, 1000);
+    
+    function mostraTempoEmTela(timeStampDoEvento) {    
+        const diaEmMs = 1000 * 60 * 60 * 24;
+        const horaEmMs = 1000 * 60 * 60;
+        const minutoEmMs = 1000 * 60;
+    
+        $("#dia").text(
+            `${Math.floor(timeStampDoEvento / diaEmMs)}d`
+        );
+        $("#hora").text(
+            `${Math.floor((timeStampDoEvento % diaEmMs) / horaEmMs)}h`
+        );
+        $("#minutos").text(
+            `${Math.floor((timeStampDoEvento % horaEmMs) / minutoEmMs)}m`
+        );
+        $("#segundos").text(
+            `${Math.floor((timeStampDoEvento % minutoEmMs) / 1000)}s`
+        );
+    }
 
-let evento = setInterval(function() {
-    const dataAtual = new Date();
-    const timeStampDataAtual = dataAtual.getTime();
-    const tempoParaEvento = timeStampEvento - timeStampDataAtual;
 
-    mostraTempoEmTela(tempoParaEvento);
-}, 1000);
+    $(".form__cadastro").click(function(e) {
+        $(e.target.parentNode).addClass("form__cadastro--is-up");
+        $(".form__cadastro__input").on("mouseleave", function(e) {
+            if($(e.target).val() == "") {
+                $(e.target.parentNode).removeClass("form__cadastro--is-up");
+            }
+        });
+    });
 
-function mostraTempoEmTela(timeStampDoEvento) {
-    const diaHtml = document.getElementById("dia");
-    const horaHtml = document.getElementById("hora");
-    const minutoHtml = document.getElementById("minutos");
-    const segundoHtml = document.getElementById("segundos");
+    $(".form").on("submit", function(e) {
+        e.preventDefault();
+    })
+    
+})
 
-    const diaEmMs = 1000 * 60 * 60 * 24;
-    const horaEmMs = 1000 * 60 * 60;
-    const minutoEmMs = 1000 * 60;
-
-    const dias = Math.floor(timeStampDoEvento / diaEmMs);
-    const horas = Math.floor((timeStampDoEvento % diaEmMs) / horaEmMs);
-    const minutos = Math.floor((timeStampDoEvento % horaEmMs) / minutoEmMs);
-    const segundos = Math.floor((timeStampDoEvento % minutoEmMs) / 1000);
-
-    diaHtml.innerHTML = `${dias}d`;
-    horaHtml.textContent = `${horas}h`;
-    minutoHtml.innerHTML = `${minutos}m`;
-    segundoHtml.innerHTML = `${segundos}s`;
-}
